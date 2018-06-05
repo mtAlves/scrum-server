4bb8f254bf0fc0e99a1cb387e42cf3144808840b
Matheus Alves <mtzin44@gmail.com>
2018-06-05 11:20:15 -0300

    remove view

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import db from './models'
-import entryPoint from './routes/entry-point'
 import { options, cors } from './config/server'
 import routes from './routes/'
 
@@ -9,13 +8,11 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.set('json spaces', 2)
-app.set('view engine', 'ejs')
 app.use(express.static('public', options))
 app.use(cors)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/', entryPoint)
 routes(app, db)
 
 db.sequelize
